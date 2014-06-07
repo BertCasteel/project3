d6a113fc4f25643265b290c86dff8832a79a1796
Kevin Beick <kbeick@zoho.com>
2014-06-07 15:43:08 -0700

    add starter code

---
+++ smsa_driver.c
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  File           : smsa_driver.c
+//  Description    : This is the driver for the SMSA simulator.
+//
+//   Author        : 
+//   Last Modified : 
+//
+
+// Include Files
+
+// Project Include Files
+#include <smsa_driver.h>
+#include <cmpsc311_log.h>
+
+// Defines
+
+// Functional Prototypes
+
+//
+// Global data
+
+// Interfaces
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : smsa_vmount
+// Description  : Mount the SMSA disk array virtual address space
+//
+// Inputs       : none
+// Outputs      : -1 if failure or 0 if successful
+
+int smsa_vmount( void ) {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : smsa_vunmount
+// Description  :  Unmount the SMSA disk array virtual address space
+//
+// Inputs       : none
+// Outputs      : -1 if failure or 0 if successful
+
+int smsa_vunmount( void )  {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : smsa_vread
+// Description  : Read from the SMSA virtual address space
+//
+// Inputs       : addr - the address to read from
+//                len - the number of bytes to read
+//                buf - the place to put the read bytes
+// Outputs      : -1 if failure or 0 if successful
+
+int smsa_vread( SMSA_VIRTUAL_ADDRESS addr, uint32_t len, unsigned char *buf ) {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : smsa_vwrite
+// Description  : Write to the SMSA virtual address space
+//
+// Inputs       : addr - the address to write to
+//                len - the number of bytes to write
+//                buf - the place to read the read from to write
+// Outputs      : -1 if failure or 0 if successful
+
+int smsa_vwrite( SMSA_VIRTUAL_ADDRESS addr, uint32_t len, unsigned char *buf )  {
+
+}
